cf30c24782b68dafc14bcf44e824ac5f7a7924a3
Chirag-wamanachara12 <chiragwamanacharya42@gmail.com>
2025-03-04 10:35:15 +0530

    changes in implicit conversion code

--- Day-3/implicitTypeConversion.cpp
+++ Day-3/implicitTypeConversion.cpp
@@ -4,18 +4,11 @@ using namespace std;
 int main(){
     char ch;
     cout<<"Enter a character: ";
-    cin>>ch;
-    if(ch >>= 65 && ch <= 90){
+    cin >> ch;
+    if(ch >= 65 && ch <= 90){
         cout<<"You entered an uppercase character."<<endl;
-    }
-    else if(ch >>= 97 && ch <= 122){
+    }else{
         cout<<"You entered a lowercase character."<<endl;
-    }
-    else if(ch >>= 48 && ch <= 57){
-        cout<<"You entered a digit."<<endl;
-    }
-    else{
-        cout<<"You entered a special character."<<endl;
     }
     return 0;
 }
